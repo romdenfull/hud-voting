0593d488235a398b5d9322a589ef2e15528eb2bb
romdenfull <127423681+romdenfull@users.noreply.github.com>
2026-05-15 17:50:41 +0300

    polls.js

--- js/polls.js
+++ js/polls.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 import { formatDate } from './utils.js';
 
