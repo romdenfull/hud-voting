d0eec97ca4a3e8a2caa506f690afdc2e7b26325c
romdenfull <127423681+romdenfull@users.noreply.github.com>
2026-05-15 17:57:07 +0300

    config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,6 @@
-export const SUPABASE_URL = 'https://taliasqdcwmcvocrxpbb.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbGlhc3FkY3dtY3ZvY3J4cGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4Mzc4NzMsImV4cCI6MjA5NDQxMzg3M30.bwh4fTAUDLezMvIH9pYb1JmCUqheFNYrdXTKeftCp34';
+// Глобальные настройки
+window.SUPABASE_URL = 'https://taliasqdcwmcvocrxpbb.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbGlhc3FkY3dtY3ZvY3J4cGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4Mzc4NzMsImV4cCI6MjA5NDQxMzg3M30.bwh4fTAUDLezMvIH9pYb1JmCUqheFNYrdXTKeftCp34';
+
+// Инициализация клиента (глобально)
+window.supabaseClient = window.supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
