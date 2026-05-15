20b26b65709fcbd19a5d68cd1bba52dede4d012e
romdenfull <127423681+romdenfull@users.noreply.github.com>
2026-05-15 15:19:32 +0300

    config.js

---
+++ js/config.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = 'https://taliasqdcwmcvocrxpbb.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbGlhc3FkY3dtY3ZvY3J4cGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4Mzc4NzMsImV4cCI6MjA5NDQxMzg3M30.bwh4fTAUDLezMvIH9pYb1JmCUqheFNYrdXTKeftCp34';
